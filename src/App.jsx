208fdf967ed1b381326b0b8fe604329dd6005114
plancookeatrepeat <debora.deprez@gmail.com>
2025-09-28 15:43:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
   );
 }
 
-export default App;() {
+export default App;
   const [query, setQuery] = useState("");
   const filtered = useMemo(
     () => RECIPES.filter((r) => (r.title + " " + r.tags.join(" "))
