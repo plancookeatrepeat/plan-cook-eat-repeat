70870f9948e583d19e6fe301c87ae4d32c421605
plancookeatrepeat <debora.deprez@gmail.com>
2025-09-28 14:09:01 +0200

    Create head.tsx

---
+++ src/head.tsx
@@ -0,0 +1,9 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta name="google-site-verification" content="<meta name="google-site-verification" content="JOUWCODE123abcXYZ" />
+ />
+    </>
+  );
+}
